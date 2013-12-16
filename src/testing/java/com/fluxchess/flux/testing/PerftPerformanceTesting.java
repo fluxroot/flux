661f78577732065f6ca503f6197b20c0e8fc7e98
Phokham Nonava <pn@nonava.com>
2013-12-16 17:44:17 +0100

    Enable testing sources

--- src/testing/java/com/fluxchess/flux/testing/PerftPerformanceTesting.java
+++ src/testing/java/com/fluxchess/flux/testing/PerftPerformanceTesting.java
@@ -34,56 +34,56 @@ import java.io.InputStreamReader;
 
 public class PerftPerformanceTesting {
 
-    private static long miniMax(Hex88Board board, MoveGenerator moveGenerator, int depth, int maxDepth) {
-        if (depth == 0) {
-            return 1;
-        }
-
-        Attack attack = board.getAttack(board.activeColor);
-        moveGenerator.initializeMain(attack, 0, IntMove.NOMOVE);
-
-        long totalNodes = 0;
-        int move = moveGenerator.getNextMove();
-        while (move != IntMove.NOMOVE) {
-            board.makeMove(move);
-            totalNodes += miniMax(board, moveGenerator, depth - 1, maxDepth);
-            board.undoMove(move);
-
-            move = moveGenerator.getNextMove();
-        }
-
-        moveGenerator.destroy();
-
-        return totalNodes;
+  private static long miniMax(Hex88Board board, MoveGenerator moveGenerator, int depth) {
+    if (depth == 0) {
+      return 1;
     }
 
-    public static void main(String[] args) {
-        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+    Attack attack = board.getAttack(board.activeColor);
+    moveGenerator.initializeMain(attack, 0, IntMove.NOMOVE);
+
+    long totalNodes = 0;
+    int move = moveGenerator.getNextMove();
+    while (move != IntMove.NOMOVE) {
+      board.makeMove(move);
+      totalNodes += miniMax(board, moveGenerator, depth - 1);
+      board.undoMove(move);
+
+      move = moveGenerator.getNextMove();
+    }
+
+    moveGenerator.destroy();
+
+    return totalNodes;
+  }
+
+  public static void main(String[] args) {
+    BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+    try {
+      String token = reader.readLine();
+      while (!token.equalsIgnoreCase("quit")) {
         try {
-            String token = reader.readLine();
-            while (!token.equalsIgnoreCase("quit")) {
-                try {
-                    Hex88Board testBoard = new Hex88Board(new GenericBoard(token));
-                    new MoveSee(testBoard);
-                    KillerTable killerTable = new KillerTable();
-                    HistoryTable historyTable = new HistoryTable();
-                    MoveGenerator moveGenerator = new MoveGenerator(testBoard, killerTable, historyTable);
+          Hex88Board testBoard = new Hex88Board(new GenericBoard(token));
+          new MoveSee(testBoard);
+          KillerTable killerTable = new KillerTable();
+          HistoryTable historyTable = new HistoryTable();
+          MoveGenerator moveGenerator = new MoveGenerator(testBoard, killerTable, historyTable);
 
-                    long startTime = System.currentTimeMillis();
-                    long result = miniMax(testBoard, moveGenerator, 6, 6);
-                    long endTime = System.currentTimeMillis();
+          long startTime = System.currentTimeMillis();
+          long result = miniMax(testBoard, moveGenerator, 6);
+          long endTime = System.currentTimeMillis();
 
-                    System.out.printf("Found %d nodes in %d.%d seconds.", result, (endTime - startTime) / 1000, (endTime - startTime) % 1000);
-                    System.out.println();
-                } catch (IllegalNotationException e) {
-                    e.printStackTrace();
-                    break;
-                }
-                token = reader.readLine();
-            }
-        } catch (IOException e1) {
-            e1.printStackTrace();
+          System.out.printf("Found %d nodes in %d.%d seconds.", result, (endTime - startTime) / 1000, (endTime - startTime) % 1000);
+          System.out.println();
+        } catch (IllegalNotationException e) {
+          e.printStackTrace();
+          break;
         }
+        token = reader.readLine();
+      }
+    } catch (IOException e1) {
+      e1.printStackTrace();
     }
+  }
 
 }
