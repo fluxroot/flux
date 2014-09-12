14950232f989a2d37047022c942709942f42419f
Phokham Nonava <pn@nonava.com>
2014-09-12 23:22:05 +0200

    Add random class

---
+++ src/main/java/com/fluxchess/flux/Random.java
@@ -0,0 +1,42 @@
+/*
+ * Copyright 2007-2014 the original author or authors.
+ *
+ * This file is part of Flux Chess.
+ *
+ * Flux Chess is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Flux Chess is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with Flux Chess.  If not, see <http://www.gnu.org/licenses/>.
+ */
+package com.fluxchess.flux;
+
+import java.security.SecureRandom;
+
+final class Random {
+
+  private static final SecureRandom random = new SecureRandom();
+
+  private Random() {
+  }
+
+  static long next() {
+    byte[] bytes = new byte[16];
+    random.nextBytes(bytes);
+
+    long hash = 0;
+    for (int i = 0; i < bytes.length; ++i) {
+      hash ^= ((long) (bytes[i] & 0xFF)) << ((i * 8) % 64);
+    }
+
+    return hash;
+  }
+
+}
